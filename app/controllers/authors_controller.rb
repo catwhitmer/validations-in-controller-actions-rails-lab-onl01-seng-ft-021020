231da45d6b3957d64319829f27513fe42383a0b7
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-18 23:05:57 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,6 +11,8 @@ class AuthorsController < ApplicationController
       if @author.valid?
         @author.save
         redirect_to author_path(@author)
+      else 
+        render :new
   end
 
   private
